ff7fb2c8f51b405ee245eb8cc027b3fe2319243c
Julius Jauga <julius.jauga@mif.stud.vu.lt>
2024-04-01 17:10:55 +0300

    Moved build folder

--- src/build-MortgageBuddy-Desktop_Qt_6_6_3_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ src/build-MortgageBuddy-Desktop_Qt_6_6_3_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../MortgageBuddy/mainwindow.h"
+#include "../../src/MortgageBuddy/mainwindow.h"
 #include <QtCore/qmetatype.h>
 
 #if __has_include(<QtCore/qtmochelpers.h>)
