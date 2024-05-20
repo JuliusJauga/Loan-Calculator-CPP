019bc293fcc9ad206d863f27f5a0f10c4bc4221a
Rokas <baliutaviciusrokas@gmail.com>
2024-05-20 14:43:42 +0300

    Added additional comments

--- src/MortgageBuddy/customQTextEdit.h
+++ src/MortgageBuddy/customQTextEdit.h
@@ -1,17 +1,40 @@
+
+/**
+ * @file customQTextEdit.h
+ * @brief Header file for the CustomQTextEdit class.
+ * 
+ * This file contains the definition of the CustomQTextEdit class, which is a custom subclass of QTextEdit, meant to allow movement between input fields using a tab press.
+ * 
+ * @author Rokas Baliutavičius
+ */
+
 #ifndef CUSTOMQTEXTEDIT_H
 #define CUSTOMQTEXTEDIT_H
 
 #include <QTextEdit>
 #include <QPushButton>
 
+/**
+ * @brief Constructor for the CustomQTextEdit class.
+*/
 class CustomQTextEdit : public QTextEdit
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructs a CustomQTextEdit object.
+     * 
+     * @param parent The parent widget.
+     */
     explicit CustomQTextEdit(QWidget *parent = nullptr);
 
 protected:
+    /**
+     * @brief Overrides the keyPressEvent() function to handle key events.
+     * 
+     * @param event The key event.
+     */
     void keyPressEvent(QKeyEvent *event) override;
 };
 
